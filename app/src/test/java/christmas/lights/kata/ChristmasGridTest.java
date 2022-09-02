a7ee52a6ad1433901dd9042b0ee5abe065a4760e
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 12:04:02 +0200

    test lenght grid added to ChristmasGridTest

--- app/src/test/java/christmas/lights/kata/ChristmasGridTest.java
+++ app/src/test/java/christmas/lights/kata/ChristmasGridTest.java
@@ -1,12 +1,22 @@
 package christmas.lights.kata;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+//import org.assertj.core.api.Assertions.*;
+
 
 class ChristmasGridTest {
+    private int[][] grid;
 
     @BeforeEach
     void setUp() {
+        grid = new ChristmasGrid().createGrid();
+    }
+
+    @Test
+    void check_the_length_of_grid(){
+        assertThat(grid.length).isEqualTo(1000);
     }
 }
