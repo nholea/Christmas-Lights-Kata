cdd19124a4aadb925e3dc53d04b20d9203b1ee6c
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 10:49:53 +0200

    added implementation in Coordinate class

--- app/src/main/java/christmas/lights/kata/Coordinate.java
+++ app/src/main/java/christmas/lights/kata/Coordinate.java
@@ -1,2 +1,21 @@
-package christmas.lights.kata;public class Coordinate {
+package christmas.lights.kata;
+
+public class Coordinate {
+    private final int x;
+    private final int y;
+
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
+
