c124e62ebce4fffcc810da2a34c5840e23967541
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 11:31:59 +0200

    Add ChristmasGridTest

---
+++ app/src/test/java/christmas/lights/kata/ChristmasGridTest.java
@@ -0,0 +1,12 @@
+package christmas.lights.kata;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChristmasGridTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
