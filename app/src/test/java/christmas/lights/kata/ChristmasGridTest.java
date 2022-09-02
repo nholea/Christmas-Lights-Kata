1f1bb20a41ee8b0f4d8f8301ac07e58edf99f2c7
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 19:03:16 +0200

    Add test turn off the middle four lights to ChristmasGridTest class

--- app/src/test/java/christmas/lights/kata/ChristmasGridTest.java
+++ app/src/test/java/christmas/lights/kata/ChristmasGridTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-
 class ChristmasGridTest {
 
     @Test
-    void check_the_length_of_grid(){
-        ChristmasGrid grid= new ChristmasGrid();
+    void check_the_length_of_grid() {
+        ChristmasGrid grid = new ChristmasGrid();
         int expectedLength = 1000;
 
         grid.createGrid();
@@ -22,45 +21,63 @@ class ChristmasGridTest {
     }
 
     @Test
-    void turn_on_all_lights_on_the_christmas_grid(){
-        Coordinate startCoordinate = new Coordinate(0,0);
-        Coordinate endCoordinate = new Coordinate(999,999);
-        ChristmasGrid grid= new ChristmasGrid();
+    void turn_on_all_lights_on_the_christmas_grid() {
+        Coordinate startCoordinate = new Coordinate(0, 0);
+        Coordinate endCoordinate = new Coordinate(999, 999);
+        ChristmasGrid grid = new ChristmasGrid();
         int expectedLightsOn = 1000000;
 
         grid.createGrid();
         grid.turnOnLights(startCoordinate, endCoordinate);
         int lightsOnResult = grid.lightsCounter();
+
         assertThat(lightsOnResult).isEqualTo(expectedLightsOn);
     }
 
     @Test
-    void given_the_coordinates_00_and_22_it_will_turn_on_9_lights(){
-        Coordinate startCoordinate = new Coordinate(0,0);
-        Coordinate endCoordinate = new Coordinate(2,2);
-        ChristmasGrid grid= new ChristmasGrid();
+    void given_the_coordinates_00_and_22_it_will_turn_on_9_lights() {
+        Coordinate startCoordinate = new Coordinate(0, 0);
+        Coordinate endCoordinate = new Coordinate(2, 2);
+        ChristmasGrid grid = new ChristmasGrid();
         int expectedLightsOn = 9;
 
         grid.createGrid();
         grid.turnOnLights(startCoordinate, endCoordinate);
         int lightsOnResult = grid.lightsCounter();
+
         assertThat(lightsOnResult).isEqualTo(expectedLightsOn);
     }
 
     @Test
-    void toggle_the_first_of_line_1000_lights(){
-        Coordinate startCoordinate= new Coordinate(0,0);
-        Coordinate endCoordinate= new Coordinate(999,0);
-        ChristmasGrid grid= new ChristmasGrid();
+    void toggle_the_first_of_line_1000_lights() {
+        Coordinate startCoordinate = new Coordinate(0, 0);
+        Coordinate endCoordinate = new Coordinate(999, 0);
+        ChristmasGrid grid = new ChristmasGrid();
         int expectedLightsOn = 1000;
 
         grid.createGrid();
         grid.toggleLights(startCoordinate, endCoordinate);
         int lightsOnResult = grid.lightsCounter();
+
         assertThat(lightsOnResult).isEqualTo(expectedLightsOn);
     }
 
+    @Test
+    void turn_off_middle_four_lights() {
+        Coordinate startCoordinateTurnOn = new Coordinate(0, 0);
+        Coordinate endCoordinateTurnOn = new Coordinate(999, 999);
+        Coordinate startCoordinateTurnOff = new Coordinate(499, 499);
+        Coordinate endCoordinateTurnOff = new Coordinate(500, 500);
+        ChristmasGrid grid = new ChristmasGrid();
+        int expectedLightsOn = 999996;
 
+        grid.createGrid();
+        grid.turnOnLights(startCoordinateTurnOn, endCoordinateTurnOn);
+        grid.turnOffLights(startCoordinateTurnOff, endCoordinateTurnOff);
+        int lightsOnResult = grid.lightsCounter();
+
+        assertThat(lightsOnResult).isEqualTo(expectedLightsOn);
+    }
 
 
 }
