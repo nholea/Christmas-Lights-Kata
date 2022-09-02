0cbdc15c398b59eba2510d4d5a4efb86349fc6f8
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 12:05:03 +0200

    create grid added to ChristmasGrid class

--- app/src/main/java/christmas/lights/kata/ChristmasGrid.java
+++ app/src/main/java/christmas/lights/kata/ChristmasGrid.java
@@ -1,2 +1,27 @@
-package christmas.lights.kata;public class ChristmasGrid {
+package christmas.lights.kata;
+
+
+public class ChristmasGrid {
+
+    private int [][] christmasGrid;
+
+    public ChristmasGrid(){
+        this.christmasGrid = new int[1000][1000];
+
+    }
+
+    public int[][] createGrid(){
+        for (int row=0; row< christmasGrid[0].length; row++){
+            for (int col=0; col< christmasGrid[1].length; col++){
+                christmasGrid[row][col] = new ChristmasLights().getStatus();
+            }
+        }
+        return christmasGrid;
+    }
+
+    public static void main(String[] args){
+        ChristmasGrid grid = new ChristmasGrid();
+        System.out.println(grid.createGrid().length);
+    }
+
 }
