9dd54dee231ee1f59e8a7c327063851481cdd423
Natalia <natalia.hernandez@clarity.ai>
2022-09-02 10:56:18 +0200

    Add ChristmasLights class

--- app/src/main/java/christmas/lights/kata/ChristmasLights.java
+++ app/src/main/java/christmas/lights/kata/ChristmasLights.java
@@ -1,2 +1,20 @@
-package christmas.lights.kata;public class ChristmasLights {
+package christmas.lights.kata;
+
+import java.util.Random;
+
+public class ChristmasLights {
+    private int status;
+    Random random = new Random();
+
+    public ChristmasLights() {
+        this.status = random.nextInt(2);
+    }
+
+    public int getStatus() {
+        return status;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+
+    }
 }
